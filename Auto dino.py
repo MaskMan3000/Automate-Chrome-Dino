7612e8c235b7b8991af991ace77fc06200046925
Mohan Chavhan <157305465+MaskMan3000@users.noreply.github.com>
2024-01-23 19:04:11 +0530

    Update Auto dino.py,Added Comments for User

--- Auto dino.py
+++ Auto dino.py
@@ -1,102 +1,3 @@
-# Version 1.0
-"""
-import pyautogui
-from PIL import ImageGrab
-import time
-
-
-class Operations:
-    x = 315
-    present_time = time.time()
-    @classmethod
-    def increase_streak_length(cls):
-        current_time = time.time()
-        elapsed_time = current_time - cls.present_time
-        if elapsed_time >= 10:
-            cls.x += 10
-            cls.present_time = current_time
-
-    def check_theme(self):
-        for i in range(50,100):
-            for j in range(170,220):
-                if data[i,j] > 100:
-                    return "White"
-                else:
-                    return "Black"
-    @classmethod
-    def hit_up(cls):
-        pyautogui.press('up')
-        time.sleep(0.08)
-        pyautogui.press('down')
-        return
-
-    @classmethod
-    def hit_down(cls):
-        pyautogui.keyDown("down")
-        time.sleep(0.08)
-        pyautogui.keyUp("down")
-        return
-
-class White(Operations):
-    def Collide_with_cactus_white(self,data):
-        for i in range(200, self.x):
-            for j in range(510, 535):
-                if data[i, j] < 100:
-                    return True
-        return False
-
-    def Collide_with_bird_white(self,data):
-        for i in range(220, (self.x - 25)):
-            for j in range(440, 460):
-                if data[i, j] < 100:
-                    return True
-        return False
-
-class Black(Operations):
-    def Collide_with_cactus_black(self,data):
-        for i in range(200, self.x):
-            for j in range(510, 535):
-                if data[i, j] > 150:
-                    return True
-        return False
-
-    def Collide_with_bird_black(self,data):
-        for i in range(220, (self.x - 25)):
-            for j in range(440, 460):
-                if data[i, j] > 150:
-                    return True
-        return False
-
-class Dino(White,Black):
-    def __init__(self,data):
-        self.data = data
-    def run(self):
-        self.increase_streak_length()
-        if self.check_theme() == "White":
-            if self.Collide_with_cactus_white(data):
-                self.hit_up()
-            elif self.Collide_with_bird_white(data):
-                self.hit_down()
-        elif self.check_theme() == "Black":
-            if self.Collide_with_cactus_black(data):
-                self.hit_up()
-            elif self.Collide_with_bird_black(data):
-                self.hit_down()
-
-
-if __name__ == '__main__':
-    time.sleep(2)
-    pyautogui.press('space')        #To start the game
-    while True:
-        image = ImageGrab.grab().convert('L')
-        data = image.load()
-        # image.show()
-        dino = Dino(data)
-        dino.run()
-
-"""
-# Version 2.0
-"""
 import pyautogui
 from PIL import ImageGrab
 import time
@@ -168,9 +69,27 @@ if __name__ == '__main__':
         data = image.load()
         dino = Dino(data)
         dino.run()
-"""
-# image = ImageGrab.grab().convert('L')
-# data = image.load()
-# Collide_with_bird(data)
-# Collide_with_cactus(data)
-# image.show()
+
+# Comment code is used to find the pixel range for your machine, By this code works perfectly for you
+    # image = ImageGrab.grab().convert('L')
+    # data = image.load()
+    
+    # To find the pixel range for the "Collide_with_cactus" Function
+    # for i in range(200, 315):                 # Note here 315 = cls.x for me
+    #         for j in range(510, 535):
+    #             if (data[i, j] < 100):
+    #                 data[i,j] = 0
+    
+    #  To find the pixel range for the "Collide_with_bird" Function
+    # for i in range(220, (315 - 25)):
+    #         for j in range(440, 460):
+    #             if (data[i, j] < 100):
+    #                 data[i,j] = 0
+        
+    # To find the pixel range for the "check_theme" Function  
+    # for i in range(50,100):
+    #         for j in range(170,220):
+    #             if data[i,j] <  100:
+    #                 data[i,j] = 0
+    
+    # image.show() 
